3d8ac9881496ea092947646cf58f9a9ce255642b
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-03-24 16:15:48 -0400

    Add basic markdown outline

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,23 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}\n
+  ## Description\n
 
+  ## Table of Contents\n
+
+  ## Installation\n
+
+  ## Usage\n
+
+  ## License\n
+
+  ## Contributing\n
+
+  ## Tests\n
+
+  ## Questions\n
 `;
 }
 

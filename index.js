d1d6b63ddffbe73075a2f4de8e72862499f8a8b6
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-03-24 11:14:16 -0400

    create a function to write README file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// TODO: Include packages needed for this application
+// Node.js Packages
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [`What is the title of your README?`];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
